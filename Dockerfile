5c9df59bf6031925806fc543c404a77d0bfce324
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-10-31 20:24:07 -0700

    update to use base scratch

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,6 @@ MAINTAINER Ivan Porto Carrero <ivan@flanders.co.nz>
 ADD ./install-caddy.sh /install-caddy.sh
 RUN /install-caddy.sh && rm /install-caddy.sh
 
-# add apaches mime.types
-ADD https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types /etc/mime.types
-
 # extract executable to root
 ADD Caddyfile /etc/caddy/Caddyfile
 
--- install-caddy.sh
+++ install-caddy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add --no-cache --purge ca-certificates shared-mime-info jq curl tar gzip
+apk add --no-cache --purge ca-certificates mailcap shared-mime-info jq curl tar gzip
 latestv=$(curl -s https://api.github.com/repos/mholt/caddy/releases/latest | jq -r .tag_name)
 curl -sL https://github.com/mholt/caddy/releases/download/$latestv/caddy_linux_amd64.tar.gz | tar -xz
 apk del --no-cache --purge curl tar gzip jq
